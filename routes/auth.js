d02c264ad327f309fd887da2b6c7f64483c1622b
Grayson Venters <graysonventers@gmail.com>
2020-12-02 09:22:30 -0800

    removed password from return json in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const User = require('../models/User');
 //  @access     private
 router.get('/', auth, async (req, res) => {
     try {
-        const user = await User.findById(req.user.id);
+        const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch (err) {
         console.error(err.message);
