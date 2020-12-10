4528164d7ef78d5ccad869c8cbf3c30083cbd5cb
Grayson Venters <graysonventers@gmail.com>
2020-12-10 14:33:06 -0800

    completed register component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Dashboard from './components/layout/Dashboard';
+import Register from './components/auth/Register';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Dashboard />
+      {/* <Dashboard /> */}
       {/* <Landing /> */}
+      <Register />
       <Footer />
     </div>
   );
