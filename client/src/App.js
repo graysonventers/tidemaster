403a8d693eb13c715b18527dc5b59ca93b74cb15
Grayson Venters <graysonventers@gmail.com>
2020-12-09 17:53:57 -0800

    working on favorites cards in dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Dashboard />
-      {/* <Landing /> */}
+      {/* <Dashboard /> */}
+      <Landing />
       <Footer />
     </div>
   );
--- client/src/components/layout/Dashboard.js
+++ client/src/components/layout/Dashboard.js
@@ -7,30 +7,64 @@ const Dashboard = () => {
 
             {/* if user DOESN'T have favorite spots */}
             
-            <div className="row" style={{ paddingTop: '30vh' }}>
+            {/* <div className="row" style={{ paddingTop: '30vh' }}>
                     <div className="col s6 offset-s3 center">
                         <div className="card cyan darken-3">
                             <div className="card-content white-text">
-                                <span className="card-title">Your Surf Spots</span>
-                                <p>Once you add one or more favorite surf spots they will be listed here. Get started!</p>
+                                <span className="card-title">Your Top Spots</span>
+                                <p>Add one or more of your favorite surf spots</p>
                             </div>
                             <section>
                             <div className="card-action">
                                 <a href="#reports"><i className="material-icons" style={{ verticalAlign: 'middle' }}>add</i><span style={{ verticalAlign: 'middle' }}>Add favorites</span></a>
                             </div>
                             </section>
-                            
                         </div>
+                    </div>
+                </div> */}
+                
+            {/* if user DOES have favorite spots */}
+            <div className="container" style={{ paddingTop: '10px' }}>
+                <a href="#surfspot">
+                    <div className="card black-text center valign-wrapper" style={{ opacity: '80%' }}>
+                        <div className="container">
+                        <div className="row">
+                                <div className="col s3"><h5>Trails</h5></div>
+                                <div className="col s3">3-4ft</div>
+                                <div className="col s3">Fair to Good</div>
+                                <div className="col s3">
+                                    <button>
+                                        <i className="material-icons tiny">clear</i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                            
+                    </div>
+                </a>
+                
+                <div className="card horizontal">
+                    <div className="card-content">
+                        <h5>Staircase</h5>
+                    </div>
+                </div>
+                <div className="card horizontal">
+                    <div className="card-content">
+                        <h5>Shitpipe</h5>
                         
                     </div>
                 </div>
-            
-            
-            
-            
+                <div className="card horizontal">
+                    <div className="card-content">
+                        <h5>El Porto</h5>
+                    </div>
+                </div>
+            </div>
             
                 
-            {/* if user DOES have favorite spots */}
+                
+            
+            
 
 
             
