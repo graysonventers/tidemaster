535c223b87a61e86e7f2d305b3dc3281509d5d89
Grayson Venters <graysonventers@gmail.com>
2020-12-11 13:52:19 -0800

    Tide component clean up

--- client/src/components/reports/Tide.js
+++ client/src/components/reports/Tide.js
@@ -6,62 +6,34 @@ const Tide = () => {
             <div className="card">
                 <div className="card-title center">Tides (ft)</div>
                 <div className="card-content">
-                <table className="striped centered">
-                <tbody className="">
-                    <tr>
-                        <td>
-                            High
-                        </td>
-                        <td>
-                            5:17am
-                        </td>
-                        <td>
-                            4.5 ft
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Low
-                        </td>
-                        <td>
-                            11:17am
-                        </td>
-                        <td>
-                            2.1 ft
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            High
-                        </td>
-                        <td>
-                            4:34pm
-                        </td>
-                        <td>
-                            3.9 ft
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Low
-                        </td>
-                        <td>
-                            10:34pm
-                        </td>
-                        <td>
-                            0.6 ft
-                        </td>
-                    </tr>
-                
-                </tbody>
-            </table>
+                    <table className="striped centered">
+                        <tbody className="">
+                            <tr>
+                                <td>High</td>
+                                <td>5:17am</td>
+                                <td>4.5 ft</td>
+                            </tr>
+                            <tr>
+                                <td>Low</td>
+                                <td>11:17am</td>
+                                <td>2.1 ft</td>
+                            </tr>
+                            <tr>
+                                <td>High</td>
+                                <td>4:34pm</td>
+                                <td>3.9 ft</td>
+                            </tr>
+                            <tr>
+                                <td>Low</td>
+                                <td>10:34pm</td>
+                                <td>0.6 ft</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            
             </div>
-            
-
         </div>
     )
-}
+};
 
 export default Tide;
