1f918936a789feaf55bbac7ffd6223a7b1529dc7
Grayson Venters <graysonventers@gmail.com>
2021-07-21 15:45:07 -0700

    added basic search functionality by surfSpot name

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,17 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../layout/Navbar';
 import Footer from '../layout/Footer';
 
+import dataCollection from '../../sampleData/dataCollection.json'
+
 const Search = () => {
+
+    // set surfspot names to an array to use to search
+    let surfSpotsList = [];
+    dataCollection.forEach(item => surfSpotsList.push([item.name, item.surfSpotId]));
+
+    const [surfSpots, setSurfSpots] = useState([]);
+
+    const onChange = e => {
+        
+        const filtered = surfSpotsList.filter(spot => {
+            return spot[0].toLowerCase().includes(e.target.value.toLowerCase())
+        })
+
+        setSurfSpots(e.target.value !== '' ? filtered : []);
+    }
+
     return (
         <Fragment>
             <Navbar />
             <div className="backgroundPrimary">
                 <nav>
                     <div className="nav-wrapper">
-                        <form>
+                        <form onSubmit={e => e.preventDefault()}>
                             <div className="input-field">
-                            <input id="search" type="search" required></input>
+                            <input id="search" type="search" required placeholder="search for surfspots..." onChange={onChange} />
                             <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
                             <i className="material-icons">close</i>
                             </div>
@@ -21,11 +40,10 @@ const Search = () => {
                 <div className="container section center">
                     <div className="card" style={{ opacity: '80%' }}>
                         <div className="card-content">
-                            <span className="card-title center">Search surf spots...</span><br></br>
-                            <span>Actual results</span><br></br><br></br>
-                            <span>Actual results</span><br></br><br></br>
-                            <span>Actual results</span><br></br><br></br>
-                            <span>Actual results</span>
+
+                            <span className="card-title center">Search Results</span><br></br>
+                            {surfSpots.map(item => <span key={item[1]}><Link to={`/reports/${item[1]}`}>{item[0]}</Link><br></br></span>)}
+                            
                         </div>
                     </div>
                 </div>
