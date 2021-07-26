a248149d683b0dfb20c91c9547a9a69f3f695c8b
Grayson Venters <graysonventers@gmail.com>
2021-07-26 13:20:13 -0700

    added useFindAndModify to mongoose connect options

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         await mongoose.connect(config.get('mongoURI'), 
         {   useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true
+            useCreateIndex: true,
+            useFindAndModify: false
         });
         console.log('MongoDB Connected')
     } catch (err) {
